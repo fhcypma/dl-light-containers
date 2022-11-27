505fb0434a461ccc41f1d5733b1fa8a15bd87470
Frank Ypma <frank.ypma@ypma-data.nl>
2022-11-27 15:29:57 +0100

    Removed test app

--- dl_light_containers/cdk.py
+++ dl_light_containers/cdk.py
@@ -2,22 +2,22 @@
 
 import aws_cdk as cdk
 from spark_on_lambda_repo_stack import SparkOnLambdaRepoStack
-from dl_light_infra.dataset_app import create_dataset_app
+# from dl_light_infra.dataset_app import create_dataset_app
 from config import settings
 
 
 app = cdk.App()
 
-create_dataset_app(
-    scope=app,
-    name="container-test",
-    env=settings.current_env,
-    etl_account=settings.aws.accounts.deploy,
-    data_account=settings.aws.accounts.deploy,
-    ecr_repository_arn=f"arn:aws:ecr:eu-west-1:{settings.aws.accounts.deploy}:repository/spark-on-lambda",
-    etl_image_version=settings.image_tag,
-    tags=settings.aws.tags,
-)
+# create_dataset_app(
+#     scope=app,
+#     name="container-test",
+#     env=settings.current_env,
+#     etl_account=settings.aws.accounts.deploy,
+#     data_account=settings.aws.accounts.deploy,
+#     ecr_repository_arn=f"arn:aws:ecr:eu-west-1:{settings.aws.accounts.deploy}:repository/spark-on-lambda",
+#     etl_image_version=settings.image_tag,
+#     tags=settings.aws.tags,
+# )
 
 deploy_env = cdk.Environment(account=settings.aws.accounts.deploy, region=settings.aws.region)
 
